4854eee255f20e804686bda7b78c4626ed645620
Alex Parrill <initrd.gz@gmail.com>
2013-03-16 18:42:26 -0400

    Added status messages, supressable with the -q flag

--- gitfograph.py
+++ gitfograph.py
@@ -8,9 +8,17 @@ if __name__ == "__main__":
 	parser.add_argument("outfile", help="SVG output")
 	parser.add_argument("-r", "--repo", default=None, help="Input repository. Defaults to current directory.")
 	parser.add_argument("-l", "--linestyle", default=4, type=int, help="Line style to use. 1 = linear, 2 = quadratic bezier, 3 and 4 = cubic beziers")
+	parser.add_argument("-q", "--quiet", help="Don't print status messages", action="store_true")
 	args = parser.parse_args()
 	
+	if not args.quiet: print("Parsing commits from log")
 	commits = gitprocess.parseLog(gitprocess.getLog(args.repo))
 	graph = grapher.SvgGraph(args.outfile, None, args.linestyle)
+	
+	if not args.quiet: print("Creating graph")
 	graph.create(commits)
+	
+	if not args.quiet: print("Writing graph to disk")
 	graph.save()
+	
+	if not args.quiet: print("Done")
